fd6c5b35eab95cf5b3bf2af175c2b8cb1781b7ce
Maid Mehic <maid.mehic@compusight.com>
2020-12-01 15:50:03 +0100

    initial

---
+++ ChatByte/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatByte
+//
+//  Created by Maid Mehic on 01/12/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
